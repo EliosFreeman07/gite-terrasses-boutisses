0e03bf77a03ac4e0ee34b105391ccb1754fcbabd
Steph <thespiderweb07@proton.me>
2026-03-27 23:09:10 +0100

    Modif transporteur

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ app.get('/api/admin/messages', verifierToken, async (req, res) => {
 });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
