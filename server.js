8bf0dd20e20d6cccc69b1a247504c60de457e257
Steph <thespiderweb07@proton.me>
2026-04-13 14:45:37 +0200

    Fix SMPT port 465

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.get('/api/admin/messages', verifierToken, async (req, res) => {
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
